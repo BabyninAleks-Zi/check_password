f77a71fa62a4cb9e561e8bb1b41e5aa74a9a7251
Aleksandr Babynin <babynin.home@gmail.com>
2025-11-17 17:43:42 +1100

    Улучшил код с добавлением функции ifmain

--- input.py
+++ input.py
@@ -1,38 +1,35 @@
-PASSWORD = input('Введите пароль:')
-SCORE = 'Рейтинг пароля:'
+def check_password(password):
+    score = 'Рейтинг пароля:'
+
+    def is_very_long(password):
+        return len(password) > 8
+
+    def has_digit(password):
+        return any(symbol.isdigit() for symbol in password)
+
+    def has_upper_letters(password):
+        return any(symbol.isupper() for symbol in password)
+
+    def has_lower_letters(password):
+        return any(symbol.islower() for symbol in password)
+
+    def has_symbols(password):
+        return any(not symbol.isdigit() and
+            not symbol.isalpha() for symbol in password)
+
+    list_rating = [
+        is_very_long,
+        has_digit,
+        has_upper_letters,
+        has_lower_letters,
+        has_symbols
+    ]
+    rating = 0
+    for rating_result in list_rating:
+        if rating_result(password):
+            rating += 2
+    print(score, rating)
 
 
-def is_very_long(PASSWORD):
-    return len(PASSWORD) > 8
-
-
-def has_digit(PASSWORD):
-    return any(symbol.isdigit() for symbol in PASSWORD)
-
-
-def has_upper_letters(PASSWORD):
-    return any(symbol.isupper() for symbol in PASSWORD)
-
-
-def has_lower_letters(PASSWORD):
-    return any(symbol.islower() for symbol in PASSWORD)
-
-
-def has_symbols(PASSWORD):
-    return any(not symbol.isdigit() and
-        not symbol.isalpha() for symbol in PASSWORD)
-
-
-list_rating = [
-    is_very_long,
-    has_digit,
-    has_upper_letters,
-    has_lower_letters,
-    has_symbols
-]
-rating = 0
-for rating_result in list_rating:
-    if rating_result(PASSWORD):
-        rating += 2
-
-print(SCORE, rating)
+if __name__ == '__main__':
+    check_password(input('Введите пароль:'))
